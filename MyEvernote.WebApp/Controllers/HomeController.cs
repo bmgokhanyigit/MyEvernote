90a42b94539468a043793de6d613710dc2bc72ee
bmgokhanyigit <bmgokhanyigit@gmail.com>
2017-04-20 16:59:57 +0300

    Myevernote.WebApp folder adding

---
+++ MyEvernote.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEvernote.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            BusinessLayer.Test test = new BusinessLayer.Test(); 
+            //test.InsertTest();
+            //test.UpdateTest();
+            //test.DeleteTest();
+
+            test.CommentTest();
+            return View(); 
+        }
+    }
+}
